36aae2baa6b12f77bdafb41ebfa4411ee41a5fde
EreaBlakaj <ereablakaj@gmail.com>
2024-05-21 21:58:19 +0200

    enkriptimi duke perdorur AES

--- src/GreetingClient.java
+++ src/GreetingClient.java
@@ -3,23 +3,18 @@ import java.io.*;
 import java.security.*;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class GreetingClient {
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
-    public static String decrypt(String cipherText, int shiftKey) {
-        cipherText = cipherText.toLowerCase();
-        StringBuilder message = new StringBuilder();
-        for (int ii = 0; ii < cipherText.length(); ii++) {
-            int charPosition = ALPHABET.indexOf(cipherText.charAt(ii));
-            int keyVal = (charPosition - shiftKey) % 26;
-            if (keyVal < 0) {
-                keyVal = ALPHABET.length() + keyVal;
-            }
-            char replaceVal = ALPHABET.charAt(keyVal);
-            message.append(replaceVal);
-        }
-        return message.toString();
+    public static String decryptAES(byte[] encryptedMessage, byte[] key) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] decryptedBytes = cipher.doFinal(encryptedMessage);
+        return new String(decryptedBytes);
     }
 
     public static boolean verifySignature(String message, String signature, PublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
@@ -37,24 +32,29 @@ public class GreetingClient {
         return keyFactory.generatePublic(spec);
     }
 
+    public static byte[] hashSharedSecret(double sharedSecret) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(Double.toString(sharedSecret).getBytes());
+        byte[] key = new byte[16]; // Use the first 16 bytes (128 bits) for AES key
+        System.arraycopy(hash, 0, key, 0, 16);
+        return key;
+    }
+
     public static void main(String[] args) {
         try {
             String pstr, gstr, Astr;
             String serverName = "localhost";
             int port = 8088;
 
-            // Declare p, g, and Key of client
             int p = 23;
             int g = 9;
             int a = 4;
             double Adash, serverB;
 
-            // Established the connection
             System.out.println("Connecting to " + serverName + " on port " + port);
             Socket client = new Socket(serverName, port);
             System.out.println("Just connected to " + client.getRemoteSocketAddress());
 
-            // Sends the data to client
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
 
@@ -68,37 +68,30 @@ public class GreetingClient {
             Astr = Double.toString(A);
             out.writeUTF(Astr); // Sending A
 
-            // Client's Private Key
             System.out.println("From Client : Private Key = " + a);
 
-            // Accepts the data
             DataInputStream inFromServer = new DataInputStream(client.getInputStream());
 
             serverB = Double.parseDouble(inFromServer.readUTF());
             System.out.println("From Server : Public Key = " + serverB);
 
             Adash = ((Math.pow(serverB, a)) % p); // calculation of Adash
-
             System.out.println("Secret Key to perform Symmetric Encryption = " + Adash);
 
-            // Create input stream to receive messages from the server
             BufferedReader inFromServerMsg = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            // Read encrypted message, signature, and public key from the server
             String encryptedMessage = inFromServerMsg.readLine();
             String signature = inFromServerMsg.readLine();
             String base64PublicKey = inFromServerMsg.readLine();
 
-            // Get the server's public key
             PublicKey publicKey = getPublicKeyFromBase64(base64PublicKey);
 
-            // Verify the signature
             boolean isVerified = verifySignature(encryptedMessage, signature, publicKey);
             if (isVerified) {
                 System.out.println("Signature valid. Trusted communication established.");
-                int i = (int) Adash;
-                String decryptedMessage = decrypt(encryptedMessage, i);
-                System.out.println("Encrypted message from server: " + encryptedMessage);
+                byte[] sharedSecretKey = hashSharedSecret(Adash); // Properly hashed shared secret for AES key
+                byte[] encryptedMessageBytes = Base64.getDecoder().decode(encryptedMessage);
+                String decryptedMessage = decryptAES(encryptedMessageBytes, sharedSecretKey);
                 System.out.println("Decrypted message from server: " + decryptedMessage);
             } else {
                 System.out.println("Signature invalid. Communication not trusted.");
--- src/GreetingServer.java
+++ src/GreetingServer.java
@@ -2,23 +2,17 @@ import java.net.*;
 import java.io.*;
 import java.security.*;
 import java.util.Base64;
-
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class GreetingServer {
-
-    private static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
     private static KeyPair keyPair;
 
-    public static String encrypt(String message, int shiftKey) {
-        message = message.toLowerCase();
-        StringBuilder cipherText = new StringBuilder();
-        for (int ii = 0; ii < message.length(); ii++) {
-            int charPosition = ALPHA.indexOf(message.charAt(ii));
-            int keyVal = (shiftKey + charPosition) % 26;
-            char replaceVal = ALPHA.charAt(keyVal);
-            cipherText.append(replaceVal);
-        }
-        return cipherText.toString();
+    public static byte[] encryptAES(String message, byte[] key) throws Exception {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        return cipher.doFinal(message.getBytes());
     }
 
     public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
@@ -35,36 +29,35 @@ public class GreetingServer {
         return Base64.getEncoder().encodeToString(signature);
     }
 
+    public static byte[] hashSharedSecret(double sharedSecret) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(Double.toString(sharedSecret).getBytes());
+        byte[] key = new byte[16]; // Use the first 16 bytes (128 bits) for AES key
+        System.arraycopy(hash, 0, key, 0, 16);
+        return key;
+    }
+
     public static void main(String[] args) throws IOException {
         try {
             int port = 8088;
+            keyPair = generateKeyPair();  // Generate RSA key pair
 
-            // Generate the server's key pair
-            keyPair = generateKeyPair();
+            int b = 3;  // Server's private key
 
-            // Server Key
-            int b = 3;
-
-            // Client p, g, and key
             double clientP, clientG, clientA, B, Bdash;
             String Bstr;
 
-            // Established the Connection
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
             Socket server = serverSocket.accept();
             System.out.println("Just connected to " + server.getRemoteSocketAddress());
 
-            // Server's Private Key
-            System.out.println("From Server : Private Key = " + b);
-
-            // Accepts the data from client
             DataInputStream in = new DataInputStream(server.getInputStream());
 
-            clientP = Integer.parseInt(in.readUTF()); // to accept p
+            clientP = Double.parseDouble(in.readUTF()); // to accept p
             System.out.println("From Client : P = " + clientP);
 
-            clientG = Integer.parseInt(in.readUTF()); // to accept g
+            clientG = Double.parseDouble(in.readUTF()); // to accept g
             System.out.println("From Client : G = " + clientG);
 
             clientA = Double.parseDouble(in.readUTF()); // to accept A
@@ -73,37 +66,32 @@ public class GreetingServer {
             B = ((Math.pow(clientG, b)) % clientP); // calculation of B
             Bstr = Double.toString(B);
 
-            // Sends data to client
-            // Value of B
             OutputStream outToClient = server.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToClient);
 
             out.writeUTF(Bstr); // Sending B
 
             Bdash = ((Math.pow(clientA, b)) % clientP); // calculation of Bdash
-
             System.out.println("Secret Key to perform Symmetric Encryption = " + Bdash);
 
-            // Create output stream to send messages to the client
-            PrintWriter outToClientMsg = new PrintWriter(server.getOutputStream(), true);
-            int i = (int) Bdash;
-            String message = "Hellofromtheserver";
-            String encryptedMessage = encrypt(message, i);
+            byte[] sharedSecretKey = hashSharedSecret(Bdash); // Properly hashed shared secret for AES key
+            byte[] encryptedMessage = encryptAES("Hellofromtheserver", sharedSecretKey);
+            String encryptedMessageBase64 = Base64.getEncoder().encodeToString(encryptedMessage);
 
             // Sign the message
-            String signature = signMessage(encryptedMessage, keyPair.getPrivate());
+            String signature = signMessage(encryptedMessageBase64, keyPair.getPrivate());
 
-            // Send the encrypted message and the signature
-            outToClientMsg.println(encryptedMessage);
+            // Send the encrypted message, signature, and public key
+            PrintWriter outToClientMsg = new PrintWriter(server.getOutputStream(), true);
+            outToClientMsg.println(encryptedMessageBase64);
             outToClientMsg.println(signature);
             outToClientMsg.println(Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded()));
 
             server.close();
         } catch (SocketTimeoutException s) {
             System.out.println("Socket timed out!");
-        } catch (IOException | NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
